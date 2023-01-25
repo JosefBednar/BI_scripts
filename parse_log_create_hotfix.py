576340835d987ee8e070bb7d7e89226bea2fbe30
JosefBednar <JosefBednar@users.noreply.github.com>
2023-01-25 13:39:55 +0100

    Update parse_log_create_hotfix.py

--- parse_log_create_hotfix.py
+++ parse_log_create_hotfix.py
@@ -2,6 +2,8 @@ import csv
 import argparse
 import glob
 from datetime import date
+import sys,os,stat
+
 """
 First part: Go through files in a folder looking for particular string - error code. 
 The second part: Parse files and create DDL statements; 
@@ -17,7 +19,10 @@ if __name__ == '__main__':
     matchingError = 'Failure 6956'
     today = date.today()
     today = today.strftime("%y%m%d")
-    print("-- HOTFIX FOR TERADATA ALTER STATEMENTS ON COLUMNS WITH STATISTICS")
+    file_path = os.path.dirname(sys.argv[0])
+    output_file = open(file_path+"/HOTFIX_ALTER_STATISTICS"+today+".sql", 'w')
+    output_file.write("-- HOTFIX FOR TERADATA ALTER STATEMENTS ON COLUMNS WITH STATISTICS\n")
+
     for file in glob.glob(args.path+"/Error*DDL*"):
         file1 = open(file, 'r')
         Lines = file1.readlines()
@@ -32,11 +37,11 @@ if __name__ == '__main__':
                 table_name = split_matchinLine[2]
                 bkp_table_name = table_name+"_BKP"+today
                 column_name = split_matchinLine[4]
-                print("--FIXING TABLE: "+table_name)
-                print("CREATE TABLE "+bkp_table_name+" as "+table_name+" WITH DATA AND STATISTICS;")
-                print("DROP STATISTICS on "+ table_name+";")
-                print(finalLineToParse.strip())
-                print("COLLECT STATISTICS ON "+table_name+" from "+ bkp_table_name+ ";")
+                output_file.write("--FIXING TABLE: "+table_name+"\n")
+                output_file.write("CREATE TABLE "+bkp_table_name+" as "+table_name+" WITH DATA AND STATISTICS;\n")
+                output_file.write("DROP STATISTICS on "+ table_name+";\n")
+                output_file.write(finalLineToParse.strip())
+                output_file.write("COLLECT STATISTICS ON "+table_name+" from "+ bkp_table_name+ ";\n\n")
 
-
-
+    output_file.close()
+    os.chmod(file_path+"/HOTFIX_ALTER_STATISTICS"+today+".sql",0777)
